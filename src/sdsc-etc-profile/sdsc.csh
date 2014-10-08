c35147070a7f4a3647050973c8b40a9c71080d20
Jim Hayes <jhayes@sdsc.edu>
2014-10-08 15:38:01 -0700

    Avoid adding redundant entries to path variables.

--- src/sdsc-etc-profile/sdsc.csh
+++ src/sdsc-etc-profile/sdsc.csh
@@ -1,17 +1,30 @@
 # Add /opt/modulefiles/* to module search path
 foreach F (`find /opt/modulefiles -maxdepth 1 -mindepth 1 -type d`)
-  setenv MODULEPATH ${F}:${MODULEPATH}
+  if (! $?MODULEPATH ) then
+    setenv MODULEPATH ${F}
+  else if ( "$MODULEPATH" !~ *${F}* ) then
+    setenv MODULEPATH ${F}:${MODULEPATH}
+  endif
 end
 
 # Point Python to scar library
-if ($?PYTHONPATH) then
-  setenv PYTHONPATH /opt/sdsc/lib:$PYTHONPATH
-else
+if (! $?PYTHONPATH) then
   setenv PYTHONPATH /opt/sdsc/lib
+else if ( "$PYTHONPATH" !~ */opt/sdsc/lib* ) then
+  setenv PYTHONPATH /opt/sdsc/lib:$PYTHONPATH
 endif
 
 # Add sdsc scripts to path
-set path = ( /opt/sdsc/bin /opt/sdsc/sbin $path )
+if (! $?path ) then
+  set path = ( /opt/sdsc/bin /opt/sdsc/sbin )
+else
+  if ( "$path" !~ */opt/sdsc/bin* ) then
+    set path = ( /opt/sdsc/bin $path )
+  endif
+  if ( "$path" !~ */opt/sdsc/sbin* ) then
+    set path = ( /opt/sdsc/sbin $path )
+  endif
+endif
 
 # Env vars to locate SDSC resources
 setenv SDSCHOME /opt/sdsc
--- src/sdsc-etc-profile/sdsc.sh
+++ src/sdsc-etc-profile/sdsc.sh
@@ -1,17 +1,30 @@
 # Add /opt/modulefiles/* to module search path
 for F in `find /opt/modulefiles -maxdepth 1 -mindepth 1 -type d`; do
-  export MODULEPATH=${F}:${MODULEPATH}
+  if test -z "$MODULEPATH"; then
+    export MODULEPATH=${F}
+  elif ! [[ "${MODULEPATH}" =~ "${F}" ]]; then
+    export MODULEPATH=${F}:${MODULEPATH}
+  fi
 done
 
 # Point Python to sdsc library
-if test -n "$PYTHONPATH"; then
-  export PYTHONPATH=/opt/sdsc/lib:$PYTHONPATH
-else
+if test -z "${PYTHONPATH}"; then
   export PYTHONPATH=/opt/sdsc/lib
+elif ! [[ "${PYTHONPATH}" =~ "/opt/sdsc/lib" ]]; then
+  export PYTHONPATH=/opt/sdsc/lib:$PYTHONPATH
 fi
 
 # Add sdsc scripts to path
-export PATH=/opt/sdsc/bin:/opt/sdsc/sbin:$PATH
+if test -z "${PATH}"; then
+  export PATH=/opt/sdsc/bin:/opt/sdsc/sbin
+else
+  if ! [[ "${PATH}" =~ "/opt/sdsc/bin" ]]; then
+    export PATH=/opt/sdsc/bin:$PATH
+  fi
+  if ! [[ "${PATH}" =~ "/opt/sdsc/sbin" ]]; then
+    export PATH=/opt/sdsc/sbin:$PATH
+  fi
+fi
 
 # Env vars to locate SDSC resources
 export SDSCHOME=/opt/sdsc
