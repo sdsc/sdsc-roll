eac4c5498d233f20937e6a6de90404c56fc4bf45
Jim Hayes <jhayes@sdsc.edu>
2014-11-06 21:26:55 -0800

    Add BIND_[U]MOUNT macros and MODULE_LOAD_PYTHON.  Switch DESCRIBE_compiler macros to report compiler, rather than $(ROLLCOMPILER) modulefile (e.g, "icc" instead of "intel").

--- src/sdsc-devel/sdsc-Rules.mk
+++ src/sdsc-devel/sdsc-Rules.mk
@@ -8,9 +8,13 @@ __SDSCDEVEL_ROLL_MK = yes
 #
 # * Settings for CC/CXX/F77/FC make variables determined by $(COMPILERNAME)
 #
-# * DESCRIBE_{CC,CXX,F77,FC,MPI,PKG,BOOST,CUDA,EIGEN,GMP} - variables that
-#   contain recipe steps to extract and echo package information for incusion
-#   in a DESCRIPTION file.
+# * BIND_MOUNT(1,2) - macro that produces recipe steps to create dir $(1) and 
+#   bind mount it to $(2)
+#   BIND_UMOUNT(1) - macro that produces recipe steps to umount and remove $(1)
+#
+# * DESCRIBE_{CC,CXX,F77,FC,PYTHON,MPI,PKG,BOOST,CUDA,EIGEN,GMP} - variables
+#   that contain recipe steps to extract and echo package information for
+#   incusion in a DESCRIPTION file.
 #
 # * INSTALL_LICENSE_FILES - variable containing recipe steps to create a dir
 #   in $(ROOT)/$(PKGROOT) to hold package licenses and copy the list of files
@@ -22,10 +26,10 @@ __SDSCDEVEL_ROLL_MK = yes
 # * MODULE_LOAD_PACKAGE(1,2) - macro that produces recipe steps to load
 #   modulefile $(1) and echo the value of environment variable $(2)
 #
-# * MODULE_LOAD_{CC,CXX,F77,FC,MPI,PKG,BOOST,CUDA,EIGEN,GMP} - variables that
-#   contain recipe steps to load specific modulefiles
+# * MODULE_LOAD_{CC,CXX,F77,FC,PYTHON,MPI,PKG,BOOST,CUDA,EIGEN,GMP} - variables
+#   that contain recipe steps to load specific modulefiles
 #
-# * PKGROOT_BIND{MOUNT,UMOUNT} - variables that contain recipe steps to create
+# * PKGROOT_BIND_{MOUNT,UMOUNT} - variables that contain recipe steps to create
 #   $(PKGROOT) and mount it to $(ROOT)/$(PKGROOT), umount and destroy it.
 #
 # * typical-roll-test-install - build target for a standard roll-test install
@@ -56,25 +60,30 @@ else ifeq ("$(COMPILERNAME)", "pgi")
   FC = pgf90
 endif
 
-DESCRIBE_CC = echo $(ROLLCOMPILER) $(call GET_EXE_VERSION, $(CC))
-DESCRIBE_CXX = echo $(ROLLCOMPILER) $(call GET_EXE_VERSION, $(CXX))
-DESCRIBE_F77 = echo $(ROLLCOMPILER) $(call GET_EXE_VERSION, $(F77))
-DESCRIBE_FC = echo $(ROLLCOMPILER) $(call GET_EXE_VERSION, $(FC))
+BIND_MOUNT = mkdir -p -m 755 $(1) || true; mount --bind $(2) $(1)
+BIND_UMOUNT = umount $(1); rmdir -p $(1) || true
 
-DESCRIBE_MPI = echo $(ROLLMPI) $(call GET_MODULE_VERSION, $(ROLLMPI))
+DESCRIBE_CC = echo built with $(CC) $(call GET_EXE_VERSION, $(CC))
+DESCRIBE_CXX = echo built with $(CXX) $(call GET_EXE_VERSION, $(CXX))
+DESCRIBE_F77 = echo built with $(F77) $(call GET_EXE_VERSION, $(F77))
+DESCRIBE_FC = echo built with $(FC) $(call GET_EXE_VERSION, $(FC))
 
-DESCRIBE_PKG = echo $(NAME) $(VERSION) built using
+DESCRIBE_PYTHON = echo built with python $(call GET_EXE_VERSION, python)
 
-DESCRIBE_BOOST = echo boost $(call GET_MODULE_VERSION, boost)
-DESCRIBE_CUDA = echo cuda $(call GET_MODULE_VERSION, cuda)
-DESCRIBE_EIGEN = echo eigen $(call GET_MODULE_VERSION, eigen)
-DESCRIBE_GMP = echo gmp $(call GET_MODULE_VERSION, gmp)
+DESCRIBE_MPI = echo built with $(ROLLMPI) $(call GET_MODULE_VERSION, $(ROLLMPI))
 
-# Macro to extract the version from running a program (arg1) with --version
+DESCRIBE_PKG = echo $(NAME) $(VERSION)
+
+DESCRIBE_BOOST = echo built with boost $(call GET_MODULE_VERSION, boost)
+DESCRIBE_CUDA = echo built with cuda $(call GET_MODULE_VERSION, cuda)
+DESCRIBE_EIGEN = echo built with eigen $(call GET_MODULE_VERSION, eigen)
+DESCRIBE_GMP = echo built with gmp $(call GET_MODULE_VERSION, gmp)
+
+# Macro to extract the version from running $(1) with --version
 GET_EXE_VERSION = \
   `$(1) --version 2>&1 | perl -ne 'print($$1) and exit if m/(\d+(\.\d+)*)/'`
 
-# Macro to extract the version from a modulefile's (arg1) whatis text
+# Macro to extract the version from the whatis text of modulefile $(1)
 GET_MODULE_VERSION = \
   `module display $(1) 2>&1 | perl -ne 'print($$1) and exit if m/version\D*(\d+(\.\d+)*)/i'`
 
@@ -99,18 +108,17 @@ MODULE_LOAD_FC = $(call MODULE_LOAD_COMPILER, $(ROLLCOMPILER), $(FC))
 
 MODULE_LOAD_MPI = $(call MODULE_LOAD_PACKAGE, $(ROLLMPI), MPIHOME)
 
+MODULE_LOAD_PYTHON = $(call MODULE_LOAD_COMPILER, $(ROLLPY), python)
+
 MODULE_LOAD_BOOST = $(call MODULE_LOAD_PACKAGE, boost, BOOSTHOME)
 MODULE_LOAD_CMAKE = $(call MODULE_LOAD_PACKAGE, cmake, CMAKEHOME)
 MODULE_LOAD_CUDA = $(call MODULE_LOAD_PACKAGE, cuda, CUDAHOME)
 MODULE_LOAD_EIGEN = $(call MODULE_LOAD_PACKAGE, eigen, EIGENHOME)
 MODULE_LOAD_GMP = $(call MODULE_LOAD_PACKAGE, gmp, GMPHOME)
 
-PKGROOT_BIND_MOUNT = \
-  mkdir -p -m 755 $(ROOT)/$(PKGROOT); \
-  mkdir -p -m 755 $(PKGROOT) || true; \
-  mount --bind $(ROOT)/$(PKGROOT) $(PKGROOT)
+PKGROOT_BIND_MOUNT = $(call BIND_MOUNT, $(PKGROOT), $(ROOT)/$(PKGROOT))
 
-PKGROOT_BIND_UMOUNT = umount $(PKGROOT); rmdir -p $(PKGROOT) || true
+PKGROOT_BIND_UMOUNT = $(call BIND_UMOUNT, $(PKGROOT))
 
 typical-roll-test-install:
 	mkdir -p -m 755 $(ROOT)/$(PKGROOT)
