b50235fe1174cd644dc28dbb63724ee808d30a2f
Jim Hayes <jhayes@sdsc.edu>
2015-07-30 08:36:32 -0700

    Add definitions for comet-only rolls.  Note gordon-only and orphaned rolls.

--- src/sdsc-devel/CommonRolls.mk
+++ src/sdsc-devel/CommonRolls.mk
@@ -26,39 +26,45 @@ guile_PREREQS = gnucompiler gnutools
 jags_PREREQS = math R
 math_PREREQS = cmake fftw gnucompiler hdf
 mpi_PREREQS = gnutools
-ncl_PREREQS = hdf netcdf
+mrbayes_PREREQS = beagle
 netcdf_PREREQS = hdf
 octave_PREREQS = fftw hdf
+openbabel_PREREQS = cmake
 polymake_PREREQS = boost gnucompiler
 r-modules_PREREQS = netcdf R
 siesta_PREREQS = hdf netcdf
 trilinos_PREREQS = cmake
 vasp_PREREQS = fftw
+vmd_PREREQS = cmake netcdf
 vtk_PREREQS = cmake
 
 # Complete roll set categorized by ROLLCOMPILER value...
 DEFAULT_COMPILER_ROLLS = \
-  abyss amber beagle beast biotools chemistry cpmd gamess geo grace jags \
-  mpi4py nwchem octave performance polymake python R scipy siesta upc vasp vtk
+  abyss amber beagle beast biotools chemistry cipres cpmd gamess geo grace \
+  jags migrate mpi4py mpiblast mrbayes nwchem octave performance phylobayes \
+  polymake python qe R raxml scipy siesta upc vasp vtk
 MULTI_COMPILER_ROLLS = \
   atlas boost fftw fpmpi hdf math mpi netcdf trilinos
 NO_COMPILER_ROLLS = \
   beast2 blcr cilk cmake data-transfer ddt fsa gaussian gnucompiler gnutools \
-  guile hadoop idl intel mono nagios pgi r-modules rapidminer stata weka
+  guile hadoop idl intel molden mono nagios openbabel pgi r-modules \
+  rapidminer stata vmd weka
 
 # ... and again by ROLLMPI value.
 DEFAULT_MPI_ROLLS = \
-  abyss amber biotools chemistry cpmd gamess grace mpi4py nwchem performance \
-  r-modules siesta upc vasp vtk
+  abyss amber biotools chemistry cpmd gamess grace migrate mpi4py mpiblast \
+  mrbayes nwchem performance phylobayes qe r-modules raxml siesta upc \
+  vasp vtk
 MULTI_MPI_ROLLS = \
   boost fftw hdf math fpmpi netcdf trilinos
 NO_MPI_ROLLS = \
-  atlas beagle beast beast2 blcr cilk cmake data-transfer ddt fsa gaussian \
-  geo gnucompiler gnutools guile hadoop idl intel jags mono mpi nagios octave \
-  polymake pgi python R rapidminer scipy stata weka
+  atlas beagle beast beast2 blcr cilk cipres cmake data-transfer ddt fsa \
+  gaussian geo gnucompiler gnutools guile hadoop idl intel jags molden mono \
+  mpi nagios octave openbabel polymake pgi python R rapidminer scipy stata \
+  vmd weka
 
 # Rolls that support ROLLPY make var
-PYTHON_ROLLS = hdf math mpi4py scipy trilinos
+PYTHON_ROLLS = hdf math mpi4py openbabel scipy trilinos vmd
 
 ALL_ROLLS = $(sort $(DEFAULT_COMPILER_ROLLS) $(MULTI_COMPILER_ROLLS) $(NO_COMPILER_ROLLS))
 
@@ -91,28 +97,54 @@ space := $(empty) $(empty)
 $(eval $(subst $(comma)$(space),$(comma),$(DEFINE_ALL_ROLLS)))
 
 # TODO: incorporate these rolls into above lists
-#cern-roll
-#cgns-roll
-#cipres-roll
-#dppdiv-roll
-#fsl-roll
-#knime-roll
-#julia-roll
-#matlab-roll
-#migrate-roll
-#molden-roll
-#mpiblast-roll
-#mrbayes-roll
-#neuron-roll
-#p3dfft-roll
-#paraview-roll
-#phylobayes-roll
-#qchem-roll
-#qe-roll
-#rabbitmq-roll
-#raxml-roll
-#seedme-roll
-#visit-roll
-#vmd-roll
+$(call ROLLDEF,matlab)
+$(call ROLLDEF,mlnx-ofed)
+$(call ROLLDEF,xsede-stats)
 
+# TODO: Gordon only
+# abaqus - source?
+# dppdiv
+# gnu - renamed gnutools
+# jmodeltest2 - no github
+# knime
+# mopac(P) - abandoned
+# mv2profile - no github
+# p3dfft
+# plink - merged into biotools
+# qchem(P)
+# visit
+
+# TODO: orphaned?
+# boltztrap
+# cern
+# cgns
+# columbus(P) - abandoned
+# cuda - abandoned
+# dataform(D) - pre-netcdf
+# db2(P) - abandoned
+# fsl
+# garli - merged into raxml
+# gdal - merged into geo
+# geos - merged into geo
+# git - no github
+# img-storage - no github
+# julia
+# lfs(P)
+# lustre(P)
+# mkl - merged into intel
+# moab(P)
+# ncl
+# node - no github
+# neuron
+# papi - merged into performance
+# perftest
+# paraview
+# proj - merged into geo
+# rabbitmq
+# rosetta
+# seedme
+# tecplot(P) - abandoned
+# usetrax - abandoned
+# wrf - no github
+ 
 endif # __SDSCDEVEL_COMMONROLLS_MK
