fc988d2a473d9fbd4072787ef7f0f40c68faa1a1
Jim Hayes <jhayes@sdsc.edu>
2016-04-19 18:44:36 -0700

    Update roll categorization to reflect changes in build parameterization.
    Resolves APS-314.

--- src/sdsc-devel/CommonRolls.mk
+++ src/sdsc-devel/CommonRolls.mk
@@ -14,7 +14,7 @@ __SDSCDEVEL_COMMONROLLS_MK = yes
 # Implicit roll prereqs, not derived from ROLL{COMPILER,MPI,PY}
 abyss_PREREQS = boost
 beast_PREREQS = beagle
-biotools_PREREQS = boost cmake fftw gnucompiler hdf math scipy
+biotools_PREREQS = boost cmake gnucompiler math scipy
 cern_PREREQS = cmake gnucompiler
 chemistry_PREREQS = cmake fftw netcdf
 cilk_PREREQS = gnucompiler
@@ -25,19 +25,21 @@ grace_PREREQS = fftw netcdf
 gnutools_PREREQS = gnucompiler
 guile_PREREQS = gnucompiler gnutools
 jags_PREREQS = math R
-llvm_PREREQS = gnucompiler python
-math_PREREQS = cmake fftw gnucompiler hdf
+llvm_PREREQS = gnucompiler
+math_PREREQS = cmake gnucompiler hdf
 mpi_PREREQS = gnutools
 mrbayes_PREREQS = beagle
+ncar_PREREQS = hdf netcdf
 ncl_PREREQS = hdf netcdf
 netcdf_PREREQS = hdf
+neuron_PREREQS = math
 octave_PREREQS = fftw hdf
 openbabel_PREREQS = cmake
 p3dfft_PREREQS = fftw
 polymake_PREREQS = boost gnucompiler
 r-modules_PREREQS = netcdf R
-siesta_PREREQS = hdf netcdf
 scipy_PREREQS = llvm
+siesta_PREREQS = hdf netcdf
 trilinos_PREREQS = cmake
 vasp_PREREQS = fftw
 vmd_PREREQS = cmake netcdf
@@ -45,41 +47,47 @@ vtk_PREREQS = cmake
 
 # Complete roll set categorized by ROLLCOMPILER value...
 DEFAULT_COMPILER_ROLLS = \
-  abyss amber beagle beast biotools chemistry cipres cpmd dppdiv gamess geo \
-  grace jags migrate mpi4py mpiblast mrbayes ncl neuron nwchem octave \
-  performance phylobayes polymake python qe R raxml scipy siesta upc vasp vmd \
-  vtk
+  abyss amber beagle beast biotools chemistry cilk cipres cpmd dppdiv gamess \
+  geo grace jags migrate mpi4py mpiblast mrbayes ncar ncl neuron nwchem octave \
+  performance phylobayes polymake python qe R r-modules raxml scipy siesta upc \
+  vasp vmd vtk
 MULTI_COMPILER_ROLLS = \
   atlas boost fftw fpmpi hdf math mpi netcdf p3dfft trilinos
 NO_COMPILER_ROLLS = \
-  beast2 blcr cern cilk cmake data-transfer ddt fsa gaussian gnucompiler \
+  beast2 blcr cern cmake data-transfer ddt fsa gaussian gnucompiler \
   gnutools guile hadoop idl intel julia knime llvm molden mono nagios \
-  openbabel pgi r-modules rapidminer stata weka
+  openbabel pgi rapidminer stata weka
 
 # ... and again by ROLLMPI value.
 DEFAULT_MPI_ROLLS = \
-  abyss amber biotools chemistry cpmd dppdiv gamess grace migrate mpi4py \
-  mpiblast mrbayes ncl neuron nwchem performance phylobayes qe r-modules \
+  abyss amber chemistry cpmd dppdiv gamess grace migrate mpi4py \
+  mpiblast mrbayes ncar ncl neuron nwchem phylobayes qe r-modules \
   raxml siesta upc vasp vmd vtk
 MULTI_MPI_ROLLS = \
-  boost fftw hdf math fpmpi netcdf p3dfft trilinos
+  boost fftw fpmpi hdf math netcdf p3dfft performance trilinos
 NO_MPI_ROLLS = \
-  atlas beagle beast beast2 blcr cern cilk cipres cmake data-transfer ddt fsa \
-  gaussian geo gnucompiler gnutools guile hadoop idl intel jags julia knime \
-  llvm molden mono mpi nagios octave openbabel polymake pgi python R \
-  rapidminer scipy stata weka
+  atlas beagle beast beast2 biotools blcr cern cilk cipres cmake data-transfer \
+  ddt fsa gaussian geo gnucompiler gnutools guile hadoop idl intel jags julia \
+  llvm molden mono mpi nagios octave openbabel pgi polymake python R \
+  knime rapidminer scipy stata weka
+
+# Rolls that load the cuda module - presently, documentation only, since we
+# don't produce a cuda roll
+CUDA_ROLLS = amber beagle chemistry mpi scipy vasp
 
 # Rolls that support ROLLPY make var
-PYTHON_ROLLS = hdf math mpi4py neuron openbabel scipy trilinos vmd
+PYTHON_ROLLS = biotools chemistry hdf llvm math mpi4py neuron openbabel scipy trilinos vmd vtk
 
 ALL_ROLLS = $(sort $(DEFAULT_COMPILER_ROLLS) $(MULTI_COMPILER_ROLLS) $(NO_COMPILER_ROLLS))
 
 # Single-compiler rolls known to have problems w/intel.
+# Intel-compiled cilk fails to link - see README.md
 # Intel 2013 gdal compilation aborts w/an internal compiler error.
 # Intel 2013-compiled R seems to have memory problems, failing this test:
 #    tp<-svd(matrix(rnorm(250*250),250,250))
+# r-modules ROLLCOMPILER has to match that of R
 # Intel-compiled siesta segfaults
-NO_INTEL_ROLLS = geo R siesta
+NO_INTEL_ROLLS = cilk geo R r-modules siesta
 
 # Multi-compiler rolls known to have build failures w/the pgi compilers.
 NO_PGI_ROLLS = atlas boost p3dfft trilinos
