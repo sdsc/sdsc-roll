a5a8eee941cc5c162300a7ee96a782325dbcc9f4
Trevor Cooper <tcooper@sdsc.edu>
2017-07-26 06:47:00 -0700

    Added /opt/modulefiles to RPM.FILES

--- src/sdsc-etc-profile/version.mk
+++ src/sdsc-etc-profile/version.mk
@@ -4,4 +4,4 @@ RELEASE    = 0
 PKGROOT    = /etc/profile.d
 
 RPM.EXTRAS = AutoReq:No
-RPM.FILES  = "/etc/profile.d/all-sdsc.csh\\n/etc/profile.d/all-sdsc.sh"
+RPM.FILES  = "/etc/profile.d/all-sdsc.csh\\n/etc/profile.d/all-sdsc.sh\\n/opt/modulefiles"
